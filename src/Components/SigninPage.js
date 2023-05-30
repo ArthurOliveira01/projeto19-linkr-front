1b1c2c3219bf9a72a1c1b094e3d6152ca17d0ff6
Sergio Beppu <beppu.sergio@gmail.com>
2023-05-30 16:57:44 -0300

    Layout pagina de login

--- src/Components/SigninPage.js
+++ src/Components/SigninPage.js
@@ -1,6 +1,148 @@
+import { useState } from "react"
+import styled from "styled-components"
+import { Link } from "react-router-dom"
+
 export default function SigninPage() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function submit() {
+
+    }
+
     return (
-        <>
-        </>
+        <SigninPageContainer>
+            <SigninPageLeft>
+                <h2>linkr</h2>
+                <p>save, share and discover the best links on the web</p>
+            </SigninPageLeft>
+            <SigninPageRight>
+                <input placeholder="e-mail" value={email} onChange={e=>setEmail(e.target.value)}/>
+                <input placeholder="password" value={password} onChange={e=>setPassword(e.target.value)}/>
+                <But onClick={submit}>Sign In</But>
+                <Link to="/signup"><p>Switch back to login</p></Link>
+            </SigninPageRight>
+        </SigninPageContainer>
     )
 }
+
+const SigninPageContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+`
+
+const SigninPageLeft = styled.div`
+    width: 60vw;
+    height: 100vh;
+    background-color: #151515;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: center;
+    padding-left: 10vw;
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    h2 {
+        font-family: 'Passion One', cursive;
+        font-weight: 700;
+        font-size: 100px;
+    }
+
+    p {
+        font-family: 'Oswald', sans-serif;
+        font-weight: 700;
+        font-size: 45px;
+        line-height: 60px;
+        width: 25vw;
+    }
+    color: #ffffff;
+
+    @media (max-width: 800px) {
+        width: 100%;
+        height: 25vh;
+        align-items: center;
+        padding-left: 0;
+
+        h2 {
+            font-size: 70px;
+        }
+
+        p {
+            font-size: 23px;
+            line-height: 34px;
+            width: 80%;
+            text-align: center;
+        }
+        color: #ffffff;
+    }
+`
+
+const SigninPageRight = styled.div`
+    width: 40vw;
+    height: 100vh;
+    background-color: #333333;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: 60vw;
+
+    input {
+        width: 30vw;
+        height: 7vh;
+        margin: 8px 0;
+        font-family: 'Oswald', sans-serif;
+        font-weight: 700;
+        font-size: 25px;
+        color: #9f9f9f;
+        border-radius: 6px;
+        ::placeholder,
+        ::-webkit-input-placeholder {
+            padding-left: 20px;
+        }
+    }
+
+    p {
+        font-family: 'Lato', sans-serif;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 24px; 
+        color: #ffffff;
+        text-decoration: underline;
+        text-decoration-color: #ffffff;
+    }
+    color: #ffffff;
+    @media (max-width: 800px) {
+        width: 100%;
+        height: 75vh;
+        top: 25vh;
+        left: 0;
+        justify-content: flex-start;
+        padding-top: 60px;
+        input {
+            width: 90vw;
+
+        }
+    }
+`
+
+const But = styled.button`
+    width: 30.3vw;
+    height: 8vh;
+    margin: 8px 0;
+    background: ${props=>props.cor ? "#cccccc" : "#1877f2"};
+    border-radius: 6px;
+    color: #ffffff;
+
+    font-family: 'Oswald', sans-serif;
+    font-weight: 700;
+    font-size: 25px;
+
+    @media (max-width: 800px) {
+        width: 90.3vw;
+    }
+`
